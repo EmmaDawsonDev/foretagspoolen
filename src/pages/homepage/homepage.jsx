4a33294734f6a5c41cb012526e21c01fa5739569
Emma Dawson <emma.l.dawson@gmail.com>
2021-10-13 15:31:20 +0200

    Add login box to homepage

--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -12,70 +12,72 @@ const Homepage = (props) => {
   const [searchTerm, setSearchTerm] = useState("");
   const [filterByCourse, setFilterByCourse] = useState("");
   const [sortBy, setSortBy] = useState("A-Ö");
+  const [loggedInUser, setLoggedInUser] = useState(false)
 
-  const setSearch = (e) => {
-    setSearchTerm(e.target.value);
-  };
+  const setSearch = e => {
+    setSearchTerm(e.target.value)
+  }
 
-  const filterCourses = (e) => {
-    setFilterByCourse(e.target.value);
-  };
+  const filterCourses = e => {
+    setFilterByCourse(e.target.value)
+  }
 
-  const sortCourses = (e) => {
-    setSortBy(e.target.value);
-  };
+  const sortCourses = e => {
+    setSortBy(e.target.value)
+  }
 
   //Apply filters:
   //Remove hidden companies from array
-  const filterCompaniesByVisible = props.companyData.filter(
-    (company) => company.synlig === true
-  );
+  const filterCompaniesByVisible = props.companyData.filter(company => company.synlig === true)
 
   //Match by search term
-  const filteredCompaniesBySearch = filterCompaniesByVisible.filter((company) =>
+  const filteredCompaniesBySearch = filterCompaniesByVisible.filter(company =>
     company.namn.toLowerCase().includes(searchTerm.toLowerCase())
-  );
+  )
 
   //Match by course
   const filteredCompanies =
-    filterByCourse === ""
+    filterByCourse === ''
       ? filteredCompaniesBySearch
-      : filteredCompaniesBySearch.filter(
-          (company) =>
-            company.utbildningar.includes(filterByCourse) ||
-            company.utbildningar.length === 0
-        );
+      : filteredCompaniesBySearch.filter(company => company.utbildningar.includes(filterByCourse) || company.utbildningar.length === 0)
 
   //Sort filtered company list
-  let filteredAndSorted = "";
+  let filteredAndSorted = ''
 
-  if (sortBy === "A-Ö") {
-    filteredAndSorted = filteredCompanies.sort((a, b) =>
-      a.namn.localeCompare(b.namn)
-    );
+  if (sortBy === 'A-Ö') {
+    filteredAndSorted = filteredCompanies.sort((a, b) => a.namn.localeCompare(b.namn))
   }
-  if (sortBy === "Ö-A") {
-    filteredAndSorted = filteredCompanies.sort((a, b) =>
-      b.namn.localeCompare(a.namn)
-    );
+  if (sortBy === 'Ö-A') {
+    filteredAndSorted = filteredCompanies.sort((a, b) => b.namn.localeCompare(a.namn))
   }
-  if (sortBy === "timestamp") {
-    filteredAndSorted = filteredCompanies.sort(
-      (a, b) => b.timestamp - a.timestamp
-    );
+  if (sortBy === 'timestamp') {
+    filteredAndSorted = filteredCompanies.sort((a, b) => b.timestamp - a.timestamp)
   }
 
   return (
     <React.Fragment>
       <TheHeader title="Företagspoolen" />
-      <main className="homepage-wrapper">
-        <section className="search">
-          <SearchBar searchTerm={searchTerm} setSearch={setSearch} />
-          <FilterDropdown filterCourses={filterCourses} />
-          <SortDropdown sortCourses={sortCourses} />
-        </section>
-        {filteredCompanies.length > 0 ? <CompanyList companyData={filteredAndSorted} /> : <h6>Inga resultat</h6>}
-      </main>
+      {!loggedInUser && (
+        <main className="homepage-wrapper__logged-out">
+          <h3>Den här sidan är lösenordsskyddad</h3>
+          <form>
+            <label htmlFor="userPassword">Lösenord:</label>
+            <input id="userPassword" type="password" />
+            <button>Login</button>
+          </form>
+        </main>
+      )}
+
+      {loggedInUser && (
+        <main className="homepage-wrapper">
+          <section className="search">
+            <SearchBar searchTerm={searchTerm} setSearch={setSearch} />
+            <FilterDropdown filterCourses={filterCourses} />
+            <SortDropdown sortCourses={sortCourses} />
+          </section>
+          {filteredCompanies.length > 0 ? <CompanyList companyData={filteredAndSorted} /> : <h6>Inga resultat</h6>}
+        </main>
+      )}
     </React.Fragment>
   )
 };
